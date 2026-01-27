9d32c4612a5fcf6d95674a1a471b330ea281d372
Dawid <273202@student.pwr.edu.pl>
2026-01-27 17:39:30 +0100

    Dodaj pliki projektów.

---
+++ BoostAsioServer.cpp
@@ -0,0 +1,12 @@
+﻿// BoostAsioServer.cpp: definiuje punkt wejścia dla aplikacji.
+//
+
+#include "BoostAsioServer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
